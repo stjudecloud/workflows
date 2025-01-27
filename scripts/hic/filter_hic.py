0534ab8fa31ccab7dcd798d149670943fe42869d
Andrew Thrasher <adthrasher@gmail.com>
2025-01-27 16:19:15 -0500

    chore: apply suggestions from PR

--- scripts/hic/filter_hic.py
+++ scripts/hic/filter_hic.py
@@ -1,45 +1,42 @@
-
 from collections import defaultdict
 import argparse
 
+
 def get_args():
-    parser = argparse.ArgumentParser(
-        description="Filter Hi-C data.")
-    parser.add_argument(
-        "--prefix", type=str, help="Prefix for output file.")
-    parser.add_argument(
-        "--all_valid_pairs", type=str, help="All valid pairs file.")
-    parser.add_argument(
-        "--filter_pairs", type=str, help="Filter pairs file.")
-    
+    parser = argparse.ArgumentParser(description="Filter Hi-C data.")
+    parser.add_argument("--prefix", type=str, help="Prefix for output file.")
+    parser.add_argument("--all_valid_pairs", type=str, help="All valid pairs file.")
+    parser.add_argument("--filter_pairs", type=str, help="Filter pairs file.")
+
     args = parser.parse_args()
     return args
-    
+
+
 if __name__ == "__main__":
     args = get_args()
-    
-    f=open(args.filter_pairs)
-    blackID=defaultdict(int)
-    while True:
-        line=f.readline()
-        if not line:
-            break
-        cols=line.strip().split("\t")
-        ID=cols[0]
-        blackID[ID]=1
 
-    f2=open(args.all_valid_pairs)
-    outfile1=args.prefix + ".allValidPairs.filtered"
-    outfile2=args.prefix + ".allValidPairs.removed"
-    of1=open(outfile1,'w')
-    of2=open(outfile2,'w')
+    f = open(args.filter_pairs)
+    exclude_ID = defaultdict(int)
     while True:
-        line=f2.readline()
+        line = f.readline()
         if not line:
             break
-        cols=line.strip().split("\t")
-        id=cols[0]
-        if id in blackID:
+        cols = line.strip().split("\t")
+        ID = cols[0]
+        exclude_ID[ID] = 1
+
+    f2 = open(args.all_valid_pairs)
+    outfile1 = args.prefix + ".allValidPairs.filtered"
+    outfile2 = args.prefix + ".allValidPairs.removed"
+    of1 = open(outfile1, "w")
+    of2 = open(outfile2, "w")
+    while True:
+        line = f2.readline()
+        if not line:
+            break
+        cols = line.strip().split("\t")
+        id = cols[0]
+        if id in exclude_ID:
             of2.write(line)
         else:
             of1.write(line)
--- scripts/hic/qc_hic.py
+++ scripts/hic/qc_hic.py
@@ -1,35 +1,48 @@
 import os
 import argparse
 
+
 def get_args():
-    parser = argparse.ArgumentParser(
-        description="QC a Hi-C experiment.")
+    parser = argparse.ArgumentParser(description="QC a Hi-C experiment.")
     parser.add_argument(
-        "--all_valid_pairs_stats", type=str, help="All valid pairs stats file.")
+        "--all_valid_pairs_stats", type=str, help="All valid pairs stats file."
+    )
+    parser.add_argument("--pairing_stats", type=str, help="Pairing stats file.")
     parser.add_argument(
-        "--pairing_stats", type=str, help="Pairing stats file.")
+        "--mapping_stats_read1", type=str, help="Mapping stats for read 1."
+    )
     parser.add_argument(
-        "--mapping_stats_read1", type=str, help="Mapping stats for read 1.")
-    parser.add_argument(
-        "--mapping_stats_read2", type=str, help="Mapping stats for read 2.")
-    parser.add_argument(
-        "--fithichip_bed", type=str, help="FithiChIP bed file.")
-    parser.add_argument(
-        "--fithichip_q01_bed", type=str, help="FithiChIP q01 bed file.")
-    parser.add_argument(
-        "--peaks_bed", type=str, help="Peaks bed file.")
-    parser.add_argument(
-        "--prefix", type=str, help="Prefix for output file.")
-        
-    
+        "--mapping_stats_read2", type=str, help="Mapping stats for read 2."
+    )
+    parser.add_argument("--fithichip_bed", type=str, help="FithiChIP bed file.")
+    parser.add_argument("--fithichip_q01_bed", type=str, help="FithiChIP q01 bed file.")
+    parser.add_argument("--peaks_bed", type=str, help="Peaks bed file.")
+    parser.add_argument("--prefix", type=str, help="Prefix for output file.")
+
     args = parser.parse_args()
-    
+
     return args
+
 
 if __name__ == "__main__":
     args = get_args()
-    FILES = [args.all_valid_pairs_stats, args.pairing_stats, args.mapping_stats_read1, args.mapping_stats_read2]
-    VARIABLE = ["valid_interaction", "cis_shortRange", "cis_longRange", "Total_pairs_processed", "Reported_pairs", "total_R2", "mapped_R2", "total_R1", "mapped_R1"]
+    FILES = [
+        args.all_valid_pairs_stats,
+        args.pairing_stats,
+        args.mapping_stats_read1,
+        args.mapping_stats_read2,
+    ]
+    VARIABLE = [
+        "valid_interaction",
+        "cis_shortRange",
+        "cis_longRange",
+        "Total_pairs_processed",
+        "Reported_pairs",
+        "total_R2",
+        "mapped_R2",
+        "total_R1",
+        "mapped_R1",
+    ]
     RESULTS = {}
     for eachfile in FILES:
         with open(eachfile) as f:
@@ -41,22 +54,38 @@ if __name__ == "__main__":
     PERCENTAGES = {}
     COUNTS = {}
     VERDICT = {}
-    REP_VAR = ["R1_aligned", "R2_aligned", "valid_interactionPairs", "cis_shortRange", "cis_longRange"]
+    REP_VAR = [
+        "R1_aligned",
+        "R2_aligned",
+        "valid_interactionPairs",
+        "cis_shortRange",
+        "cis_longRange",
+    ]
 
     if args.fithichip_bed is not None and os.path.isfile(args.fithichip_bed):
         with open(args.fithichip_q01_bed) as fithichip:
-            LOOPS_SIGNIFICANT = len(fithichip.readlines())-1
+            LOOPS_SIGNIFICANT = len(fithichip.readlines()) - 1
         with open(args.fithichip_bed) as fithichip:
-            LOOPS = len(fithichip.readlines())-1
+            LOOPS = len(fithichip.readlines()) - 1
     if args.peaks_bed is not None and os.path.isfile(args.peaks_bed):
         with open(args.peaks_bed) as peaks:
-            PEAKS = len(peaks.readlines())-1
+            PEAKS = len(peaks.readlines()) - 1
 
-    PERCENTAGES["R1_aligned"] = round((RESULTS["mapped_R1"]*100)/RESULTS["total_R1"])
-    PERCENTAGES["R2_aligned"] = round((RESULTS["mapped_R2"]*100)/RESULTS["total_R2"])
-    PERCENTAGES["valid_interactionPairs"] = round((RESULTS["valid_interaction"]*100)/RESULTS["Reported_pairs"])
-    PERCENTAGES["cis_shortRange"] = round((RESULTS["cis_shortRange"]*100)/RESULTS["valid_interaction"])
-    PERCENTAGES["cis_longRange"] = round((RESULTS["cis_longRange"]*100)/RESULTS["valid_interaction"])
+    PERCENTAGES["R1_aligned"] = round(
+        (RESULTS["mapped_R1"] * 100) / RESULTS["total_R1"]
+    )
+    PERCENTAGES["R2_aligned"] = round(
+        (RESULTS["mapped_R2"] * 100) / RESULTS["total_R2"]
+    )
+    PERCENTAGES["valid_interactionPairs"] = round(
+        (RESULTS["valid_interaction"] * 100) / RESULTS["Reported_pairs"]
+    )
+    PERCENTAGES["cis_shortRange"] = round(
+        (RESULTS["cis_shortRange"] * 100) / RESULTS["valid_interaction"]
+    )
+    PERCENTAGES["cis_longRange"] = round(
+        (RESULTS["cis_longRange"] * 100) / RESULTS["valid_interaction"]
+    )
     COUNTS["R1_aligned"] = RESULTS["mapped_R1"]
     COUNTS["R2_aligned"] = RESULTS["mapped_R2"]
     COUNTS["valid_interactionPairs"] = RESULTS["valid_interaction"]
@@ -88,11 +117,22 @@ if __name__ == "__main__":
     else:
         VERDICT["cis_longRange"] = "BAD"
 
-    REPORT = open(args.prefix + "_QCreport.txt", 'w')
+    REPORT = open(args.prefix + "_QCreport.txt", "w")
     REPORT.write("STAT\tCOUNTS\tPERCENTAGE\tVERDICT\n")
-    REPORT.write("Total_pairs_processed\t" + str(RESULTS["Total_pairs_processed"]) + "\n")
+    REPORT.write(
+        "Total_pairs_processed\t" + str(RESULTS["Total_pairs_processed"]) + "\n"
+    )
     for var in REP_VAR:
-        REPORT.write(var + "\t" + str(COUNTS[var]) + "\t" + str(PERCENTAGES[var]) + "\t" + VERDICT[var] + '\n')
+        REPORT.write(
+            var
+            + "\t"
+            + str(COUNTS[var])
+            + "\t"
+            + str(PERCENTAGES[var])
+            + "\t"
+            + VERDICT[var]
+            + "\n"
+        )
 
     if args.peaks_bed is not None and os.path.isfile(args.peaks_bed):
         REPORT.write("peaks\t" + str(PEAKS) + "\n")
