cc6a5f6f5283e29c2c08c4909bfb8e5a6dcd1cca
Andrew Frantz <andrew.frantz@stjude.org>
2025-02-12 11:06:18 -0600

    Update scripts/util/calc_global_phred_scores.py
    Co-authored-by: Andrew Thrasher <adthrasher@gmail.com>

--- scripts/util/calc_global_phred_scores.py
+++ scripts/util/calc_global_phred_scores.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--fast_mode",
         action="store_true",
-        help="Only calculate the first, middle, and last position PHRED scores isntead of also calculating PHRED scores for _every_ position",
+        help="Only calculate the first, middle, and last position PHRED scores instead of also calculating PHRED scores for _every_ position",
     )
     args = parser.parse_args()
 
