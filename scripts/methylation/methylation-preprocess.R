5af65ae26e15cbd7852a07bcbe1517fbc5c22319
Andrew Thrasher <adthrasher@gmail.com>
2025-02-13 13:16:11 -0500

    ci: add R lint and format (#215)

--- scripts/methylation/methylation-preprocess.R
+++ scripts/methylation/methylation-preprocess.R
@@ -8,79 +8,99 @@ suppressPackageStartupMessages(library("argparse"))
 # create parser object
 parser <- ArgumentParser()
 # specify options
-parser$add_argument("--idat_base", type="character", help="Base path to the idat files")
-parser$add_argument("--out_base", type="character", help="Base path to the output files")
-parser$add_argument("--seed", type="numeric", default=1, help="Seed for random number generation")
+parser$add_argument(
+  "--idat_base",
+  type = "character",
+  help = "Base path to the idat files"
+)
+parser$add_argument(
+  "--out_base",
+  type = "character",
+  help = "Base path to the output files"
+)
+parser$add_argument(
+  "--seed",
+  type = "numeric",
+  default = 1,
+  help = "Seed for random number generation"
+)
 # parse the arguments
 args <- parser$parse_args()
 
-# These arrays have two types of probes (named Infinium type 1 and Infinium type 2).
-# The probes also cover a varying number of CpG sites.
-# The normalization method works by taking N probes of each type that each have 1, 2, or 3 CpGs.
-# So it selects 6N probes in total. This selection is "random" if the seed is not fixed, of course.
-# Since we're processing samples individually in this step instead of as a cohort,
-# we want the seed to be consistent so the same set of probes is chosen for each sample.
+# These arrays have two types of probes (named Infinium type 1 and Infinium
+# type 2). The probes also cover a varying number of CpG sites. The
+# normalization method works by taking N probes of each type that each have 1,
+# 2, or 3 CpGs. So it selects 6N probes in total. This selection is "random"
+# if the seed is not fixed, of course. Since we're processing samples
+# individually in this step instead of as a cohort. We want the seed to be
+# consistent so the same set of probes is chosen for each sample.
 set.seed(args$seed)
 
-dir=getwd()
+dir <- getwd()
 
-RGSet <- read.metharray(args$idat_base, verbose = TRUE, force = TRUE)
-saveRDS(RGSet, paste0(args$out_base, ".RGSet.rds"))
+rg_set <- read.metharray(args$idat_base, verbose = TRUE, force = TRUE)
+saveRDS(rg_set, paste0(args$out_base, ".RGSet.rds"))
 
 # The manifest is needed by preprocessRAW
-manifest <- getManifest(RGSet)
+manifest <- getManifest(rg_set)
 manifest
 
 # Load raw data into a MethylSet object be converting red/green
 # channels into a matrix of methlyated and unmethylated signals.
-MSet <- preprocessRaw(RGSet)
-saveRDS(MSet, paste0(args$out_base, ".MSet.rds"))
+m_set <- preprocessRaw(rg_set)
+saveRDS(m_set, paste0(args$out_base, ".MSet.rds"))
 
 # Convert to a RatioSet
-RSet <- ratioConvert(MSet, what = "both", keepCN = TRUE)
-saveRDS(RSet, paste0(args$out_base, ".RSet.rds"))
+r_set <- ratioConvert(m_set, what = "both", keepCN = TRUE)
+saveRDS(r_set, paste0(args$out_base, ".RSet.rds"))
 
 # Add genomic coordinates to each probe (plus additional annotation)
-GRset <- mapToGenome(RSet)
-saveRDS(GRset, paste0(args$out_base, ".GRSet.rds"))
+gr_set <- mapToGenome(r_set)
+saveRDS(gr_set, paste0(args$out_base, ".GRSet.rds"))
 
 # Take the genomic mapped RatioSet and fill Beta values (non-normalized).
 # Get the NON-normalized beta values:
-beta <- getBeta(GRset)
+beta <- getBeta(gr_set)
 write.csv(beta, paste0(args$out_base, ".beta.csv"))
 
 # Get M-value matrix and copy-number matrix
 # Get M and CN vals if interested:
-M <- getM(GRset)
-write.csv(M, paste0(args$out_base, ".m_values.csv"))
-CN <- getCN(GRset)
-write.csv(CN, paste0(args$out_base, ".cn_values.csv"))
+m <- getM(gr_set)
+write.csv(m, paste0(args$out_base, ".m_values.csv"))
+cn <- getCN(gr_set)
+write.csv(cn, paste0(args$out_base, ".cn_values.csv"))
 
 # Get sample names and probe names
-sampleNames <- sampleNames(GRset)
-write.csv(sampleNames, paste0(args$out_base, ".sampleNames.csv"))
-probeNames <- featureNames(GRset)
-write.csv(probeNames, paste0(args$out_base, ".probeNames.csv"))
+sample_names <- sampleNames(gr_set)
+write.csv(sample_names, paste0(args$out_base, ".sampleNames.csv"))
+probe_names <- featureNames(gr_set)
+write.csv(probe_names, paste0(args$out_base, ".probeNames.csv"))
 
-gr <- granges(GRset)
+gr <- granges(gr_set)
 write.csv(gr, paste0(args$out_base, ".gr.csv"))
 
-annotation <- getAnnotation(GRset)
+annotation <- getAnnotation(gr_set)
 write.csv(annotation, paste0(args$out_base, ".annotation.csv"))
 
 # Perform SWAN normalization on beta values
-GRset.swan_norm <- preprocessSWAN(RGSet)
-beta_swan_norm <- getBeta(GRset.swan_norm)
+gr_set_swan_norm <- preprocessSWAN(rg_set)
+beta_swan_norm <- getBeta(gr_set_swan_norm)
 
 # Write the normalized beta-values that have NOT yet had
 # low-variance probes filtered out
-write.csv(beta_swan_norm, paste0(args$out_base, ".beta_swan_norm_unfiltered.csv"))
+write.csv(
+  beta_swan_norm,
+  paste0(args$out_base, ".beta_swan_norm_unfiltered.csv")
+)
 
 # Write the normalized beta-values that have NOT yet had
 # low-variance probes filtered out
 # Filter to only those that are mappable to the genome.
-RSet <- ratioConvert(GRset.swan_norm)
-GRset <- mapToGenome(RSet)
-beta_swan_norm <- getBeta(GRset)
+r_set <- ratioConvert(gr_set_swan_norm)
+gr_set <- mapToGenome(r_set)
+beta_swan_norm <- getBeta(gr_set)
 colnames(beta_swan_norm) <- colnames(CN)
-write.csv(beta_swan_norm[order(rownames(beta_swan_norm)),], paste0(args$out_base, ".beta_swan_norm_unfiltered.genomic.csv"))
+write.csv(
+  beta_swan_norm[order(rownames(beta_swan_norm)), ],
+  paste0(args$out_base, ".beta_swan_norm_unfiltered.genomic.csv")
+)
