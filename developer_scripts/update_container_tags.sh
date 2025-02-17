08d5f301b7a12c8ce8ec0686187fb187f7802bea
Andrew Thrasher <adthrasher@gmail.com>
2025-02-17 11:00:11 -0500

    ci: update branch container tags for testing

--- developer_scripts/update_container_tags.sh
+++ developer_scripts/update_container_tags.sh
@@ -9,8 +9,8 @@ if [[ "$target" == "main" ]]; then
   search_pattern='container: "ghcr\.io/stjudecloud/([^:]*):branch-[^-]*-(.*)"'
   replacement='container: "ghcr.io/stjudecloud/\1:\2"'
 else
-  search_pattern='container: "ghcr\.io/stjudecloud/([^:]*):(.*)"'
-  replacement='container: "ghcr.io/stjudecloud/\1:branch-'$target'-\2"'
+  search_pattern='container: "ghcr\.io/stjudecloud/([^:]*):(branch-'$target'-)?(.*)"'
+  replacement='container: "ghcr.io/stjudecloud/\1:branch-'$target'-\3"'
 fi
 
 find . -not -path '.git' -a -type f -name '*.wdl' | xargs sed -Eri '' "s,$search_pattern,$replacement,g"
