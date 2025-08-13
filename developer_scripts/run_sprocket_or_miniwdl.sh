4e4253a36a358d0839eea99d9cf2f554e72cbb35
Ari Frantz <ari.frantz@stjude.org>
2025-08-13 15:50:23 -0400

    Update run_sprocket_or_miniwdl.sh

--- developer_scripts/run_sprocket_or_miniwdl.sh
+++ developer_scripts/run_sprocket_or_miniwdl.sh
@@ -7,18 +7,71 @@ usage_and_quit () {
     echo
     echo "If RUNNER env var is unset, sprocket will be run"
     echo
-    echo "Usage: RUNNER=<sprocket or miniwdl> $(basename "$0") [path to WDL document] [input files and key value pairs]..." 
+    echo "Usage: RUNNER=<sprocket or miniwdl> $(basename "$0") [-i <input file>] [-t|-w <task or workflow (respectively) to run>] [path to WDL document] [optional key value pairs]..." 
     exit 1
 }
 
 [[ "$#" -eq 0 ]] && usage_and_quit
-[[ "$#" -le 2 ]] && usage_and_quit
 
 runner=${RUNNER:-sprocket}
 if [ "$runner" != "sprocket" ] && [ "$runner" != "miniwdl" ]; then
-    echo "RUNNER environment variable must be 'sprocket' or 'miniwdl'"
+    echo "ERROR: RUNNER environment variable must be 'sprocket' or 'miniwdl'"
     echo
     usage_and_quit
 fi
 
+input_file=""
+task=""
+wf=""
+while getopts "hi:t:w:" option; do
+   case $option in
+      i)
+         input_file=$OPTARG
+         ;;
+      t)
+         task=$OPTARG
+         ;;
+      w)
+         wf=$OPTARG
+         ;;
+      h)
+         usage_and_quit
+         ;;
+      *)
+         echo "ERROR: unrecognized flag"
+         echo
+         usage_and_quit
+         ;;
+   esac
+done
+
+shift $((OPTIND-1))
+
 wdl=$1; shift;
+
+if [[ $task ]] && [[ $wf ]]; then
+   echo "ERROR: both -t and -w cannot be supplied"
+   echo
+   usage_and_quit
+fi
+if [ -z "$task" ] && [ -z "$wf" ]; then
+   echo "ERROR: -t or -w must be supplied"
+   echo
+   usage_and_quit
+fi
+
+if [ "$runner" != "miniwdl" ]; then
+   if [[ $task ]]; then
+      entrypoint=$task
+   else
+      entrypoint=$wf
+   fi
+   sprocket run -e "$entrypoint" "$wdl" ${input_file:+"$input_file"} "$@"
+else
+   if [[ $input_file ]]; then
+      input_dir=$(dirname "$input_file")
+      sed -e 's;../../;'"$input_dir"'/../../;g' "$input_file" > tmp.json
+      input_file=tmp.json
+   fi
+   miniwdl run ${task:+--task "$task"} ${input_file:+-i "$input_file"} "$wdl" "$@"
+fi
